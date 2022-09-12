8b0fd3b5db76752569a3816c0a31721720778e07
ahino6942 <79881161+ahino6942@users.noreply.github.com>
2022-09-11 19:07:13 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
+export default {
+  kit: {
+    // default options are shown
+    adapter: vercel({
+      // if true, will deploy the app using edge functions
+      // (https://vercel.com/docs/concepts/functions/edge-functions)
+      // rather than serverless functions
+      edge: false,
 
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+      // an array of dependencies that esbuild should treat
+      // as external when bundling functions
+      external: [],
 
-	kit: {
-		adapter: adapter({
-			precompress: true
-		})
-	}
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app
+      split: false
+    })
+  }
 };
-
-export default config;
